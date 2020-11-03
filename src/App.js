a07b6964a40f9d0ac6ed54ba283ac7ed61129efd
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-03 17:11:30 +0200

    setting up state and fetching data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleRight } from 'react-icons/fa';
+
 const url = 'https://course-api.netlify.app/api/react-tabs-project';
+
 function App() {
-  return <h2>tabs project setup</h2>;
+  // setting up state
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  // fetching data
+  const fetchJobs = async () => {
+    const reponse = await fetch(url);
+    const newJobs = await reponse.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  // fetch jobs just once
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  return <h2>jobs</h2>;
 }
 
 export default App;
